1660f7f0190286da2bb5ceca9996be18e03ec099
phhanh12 <phanh.study@gmail.com>
2025-04-17 13:52:04 +0700

    create meet the team section

---
+++ src/app/components/MeetTheTeam.tsx
@@ -0,0 +1,196 @@
+const MeetTheTeam = () => {
+  return (
+    <div className="relative">
+      <div className="absolute z-10 bg-black/70 w-full h-full rounded-xl"></div>
+      <div>
+        <img
+          className="block sm:hidden w-full h-full object-cover"
+          src="/images/bg-meet-team-mobile.png"
+          alt=""
+        />
+        <img
+          className="hidden sm:block w-full h-full object-cover"
+          src="/images/bg-meet-team.png"
+          alt=""
+        />
+      </div>
+      <div className="absolute flex flex-col justify-self-center top-0 py-7 md:w-[80%] md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 text-center text-white z-30 ">
+        <h3 className="hidden md:block xl:text-4xl md:text-2xl font-bold md:mb-2 xl:mb-7">
+          Meet the team section
+        </h3>
+        <h3 className="block md:hidden text-xl font-bold mb-3 ">
+          Meet the team
+        </h3>
+        <p className="mb-2 xl:text-xl text-sm xl:mb-9 w-[80%] mx-auto">
+          Contrary to popular belief, is not simply random text. It has roots in
+          a piece of classical Latin listerature from 45 BC, making it over 2000
+          years old.
+        </p>
+        <div className="flex flex-col md:flex-row justify-between md:mt-1 mt-5 gap-y-6">
+          <div>
+            <div className="flex justify-center">
+              <img
+                className="object-cover lg:w-full w-[50%]"
+                src="/images/team1.png"
+                alt=""
+              />
+            </div>
+            <span className="block text-md md:mt-2 mt-4">Safia Chowdhury</span>
+            <span className="block md:mt-1 text-sm">Fruit Expert</span>
+            <ul className=" flex gap-x-3 justify-center md:mt-3 mt-2">
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <img
+                className="object-cover lg:w-full w-[50%]"
+                src="/images/team1.png"
+                alt=""
+              />
+            </div>
+            <span className="block text-md md:mt-2 mt-4">Safia Chowdhury</span>
+            <span className="block md:mt-1 text-sm">Fruit Expert</span>
+            <ul className=" flex gap-x-3 justify-center md:mt-3 mt-2">
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <img
+                className="object-cover lg:w-full w-[50%]"
+                src="/images/team1.png"
+                alt=""
+              />
+            </div>
+            <span className="block text-md md:mt-2 mt-4">Safia Chowdhury</span>
+            <span className="block md:mt-1 text-sm">Fruit Expert</span>
+            <ul className=" flex gap-x-3 justify-center md:mt-3 mt-2">
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <img
+                className="object-cover lg:w-full w-[50%]"
+                src="/images/team1.png"
+                alt=""
+              />
+            </div>
+            <span className="block text-md md:mt-2 mt-4">Safia Chowdhury</span>
+            <span className="block md:mt-1 text-sm">Fruit Expert</span>
+            <ul className=" flex gap-x-3 justify-center md:mt-3 mt-2">
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer" href="#">
+                  <img
+                    className="lg:w-7 lg:h-7 w-5 h-5 p-1 bg-[#F85559] rounded-full"
+                    src="/images/icon-facebook.svg"
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MeetTheTeam;
