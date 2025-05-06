7a3809a06e4e549223e80ab70fe23ea4e69fdd87
phhanh12 <phanh.study@gmail.com>
2025-05-06 19:42:04 +0700

    define function and props for count down time

--- src/app/components/CountDown.tsx
+++ src/app/components/CountDown.tsx
@@ -1,43 +1,40 @@
 import Image from "next/image";
 
-const CountDown = () => {
+interface CountDownProps {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+const CountDown = ({ days, hours, minutes, seconds }: CountDownProps) => {
+  const renderCountDownTime = (
+    label: string,
+    value: number,
+    isShowEllipsis: boolean
+  ) =>
+    value >= 0 ? (
+      <>
+        <div className="flex flex-col items-center gap-y-1">
+          <span className="text-3xl font-bold">{value}</span>
+          <span className="text-gray-500 text-sm">{label}</span>
+        </div>
+        {showEllipsis(isShowEllipsis)}
+      </>
+    ) : null;
+
+  const showEllipsis = (showEllipsis: boolean) =>
+    showEllipsis && (
+      <Image alt="ellipsis" src="/images/ellipsis.svg" width={20} height={20} />
+    );
+
   return (
     <div className="text-center mt-6 md:w-[50%]">
       <div className="flex justify-end items-center gap-x-2 ">
-        <div className="flex flex-col items-center gap-y-1">
-          <span className="text-3xl font-bold">29</span>
-          <span className="text-gray-500 text-sm">Days</span>
-        </div>
-        <Image
-          alt="ellipsis"
-          src="/images/ellipsis.svg"
-          width={20}
-          height={20}
-        />
-        <div className="flex flex-col items-center gap-y-1">
-          <time className="text-3xl font-bold">15</time>
-          <span className="text-gray-500 text-sm">Hours</span>
-        </div>
-        <Image
-          alt="ellipsis"
-          src="/images/ellipsis.svg"
-          width={20}
-          height={20}
-        />
-        <div className="flex flex-col items-center gap-y-1">
-          <time className="text-3xl font-bold">25</time>
-          <span className="text-gray-500 text-sm">Minutes</span>
-        </div>
-        <Image
-          alt="ellipsis"
-          src="/images/ellipsis.svg"
-          width={20}
-          height={20}
-        />
-        <div className="flex flex-col items-center gap-y-1">
-          <time className="text-3xl font-bold">08</time>
-          <span className="text-gray-500 text-sm">Seconds</span>
-        </div>
+        {renderCountDownTime("Days", days, true)}
+        {renderCountDownTime("Hours", hours, true)}
+        {renderCountDownTime("Minutes", minutes, true)}
+        {renderCountDownTime("Seconds", seconds, false)}
       </div>
     </div>
   );
